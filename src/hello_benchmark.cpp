fc7ff31f3369ba1544ce191c5dec0a6e19b57ecc
SidorovichPavel <sidorovich.pasha@gmail.com>
2024-02-02 16:48:52 +0300

    formatting

--- src/hello_benchmark.cpp
+++ src/hello_benchmark.cpp
@@ -14,8 +14,7 @@ void HelloBenchmark(benchmark::State& state) {
 
     for (auto _ : state) {
       const auto name = kNames[i++ % std::size(kNames)];
-      auto result = ubank::SayHelloTo(
-          name, ubank::UserType::kFirstTime);
+      auto result = ubank::SayHelloTo(name, ubank::UserType::kFirstTime);
       benchmark::DoNotOptimize(result);
     }
   });
