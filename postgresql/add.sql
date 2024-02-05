82e136b5787ac9452b1956d2a9e55afe4e9e7398
SidorovichPavel <sidorovich.pasha@gmail.com>
2024-02-05 20:21:26 +0300

    update add querry

--- postgresql/add.sql
+++ postgresql/add.sql
@@ -1,3 +1,5 @@
-INSERT INTO clients_schema.clients(first_name, middle_name,last_name) 
-VALUES('p','g','s') 
+INSERT INTO bank_schema.clients(first_name, middle_name,last_name,gender,passport_series,passport_number,issuing,issuing_date,id_number,
+	birth_address,current_city,current_address,city_of_residence,residence_address,family_status,citizenship,retiree,monthly_income,conscription) 
+VALUES('pavel','gennadevich','sidorovich',1,'KH','2462649','Волковысский РОВД','2014-01-01','id_number',
+	'birth_address','current_city','current_address','city_of_residence','residence_address',0,'citizenship',false,100,false) 
 RETURNING id
