be3728d1ae84e1b249efe49937e6ed4a7fb746e5
SidorovichPavel <sidorovich.pasha@gmail.com>
2024-02-21 00:12:12 +0300

    refactoring

--- src/client.cpp
+++ src/client.cpp
@@ -1,7 +1,11 @@
 #include "client.hpp"
 
+#include "client_info.hpp"
+
+#include <iostream>
 #include <optional>
 #include <string>
+#include <string_view>
 
 #include <userver/clients/dns/component.hpp>
 #include <userver/components/component.hpp>
@@ -9,53 +13,19 @@
 #include <userver/server/handlers/http_handler_json_base.hpp>
 #include <userver/storages/postgres/cluster.hpp>
 #include <userver/storages/postgres/component.hpp>
+#include <userver/storages/postgres/database.hpp>
 #include <userver/utils/assert.hpp>
 
 namespace ubank {
 
 namespace {
 
-enum class Gender : std::int16_t { Female, Man, Other };
-
-enum class FamilyStatus : std::int16_t { Single, Married };
-
-enum class Disability : std::int16_t { FirstGroup, SecondGroup, ThirdGroup };
-
-struct ClientInfo {
-  std::string first_name;
-  std::string middle_name;
-  std::string last_name;
-  Gender gender;
-  std::string passport_series;
-  std::string passport_number;
-  std::string issuing;
-  std::string issuing_date;
-  std::string id_number;
-  std::string birth_address;
-  std::string current_city;
-  std::string current_address;
-  std::optional<std::string> otp_home_number;
-  std::optional<std::string> opt_mobile_number;
-  std::optional<std::string> opt_email;
-  std::optional<std::string> opt_position;
-  std::optional<std::string> opt_place_of_work;
-  std::string city_of_residence;
-  std::string residence_address;
-  FamilyStatus family_status;
-  std::string citizenship;
-  std::optional<Disability> otp_disability;
-  bool retiree;
-  int monthly_income;
-  bool conscription;
-};
-
-class HttpHandlerUser : public userver::server::handlers::HttpHandlerJsonBase {
+class HttpPostClient : public userver::server::handlers::HttpHandlerJsonBase {
  public:
-  static constexpr std::string_view kName = "handler-user";
+  static constexpr std::string_view kName = "handler-client";
 
-  HttpHandlerUser(
-      const userver::components::ComponentConfig& config,
-      const userver::components::ComponentContext& component_context)
+  HttpPostClient(const userver::components::ComponentConfig& config,
+                 const userver::components::ComponentContext& component_context)
       : HttpHandlerJsonBase(config, component_context),
         pg_cluster_(
             component_context
@@ -66,40 +36,76 @@ class HttpHandlerUser : public userver::server::handlers::HttpHandlerJsonBase {
       const userver::server::http::HttpRequest& request,
       const userver::formats::json::Value& json,
       userver::server::request::RequestContext&) const override {
-    userver::formats::json::ValueBuilder result;
-    result["status"] = 200;
+    ClientInfo info = Deserialize(json["client"]);
 
-    ClientInfo user;
-    user.first_name = json["first_name"].As<std::string>();
-    user.middle_name = json["middle_name"].As<std::string>();
-    user.last_name = json["last_name"].As<std::string>();
-    //user.gender = json["gender"].As<Gender>();
-    
-    // pg_cluster_->Execute(
-    //     userver::storages::postgres::ClusterHostType::kMaster,
-    //     "INSERT INTO bank_schema.clients(first_name, "
-    //     "middle_name,last_name,gender,passport_series,passport_number, "
-    //     "issuing,issuing_date,id_number, "
-    //     "birth_address,current_city,current_address,home_number,city_of_residence, "
-    //     "residence_address,family_status,citizenship,retiree,monthly_income, "
-    //     "conscription) "
-    //     "VALUES('pavel','gennadevich','sidorovich',1,'KH','2462649',' "
-    //     "Волковысский РОВД','2014-01-01','id_number', "
-    //     "'birth_address','current_city','current_address',$1,'city_of_residence' "
-    //     ",'residence_address',0,'citizenship',false,100,false) "
-    //     "RETURNING id",user.otp_home_number);
+    auto pg_result = pg_cluster_->Execute(
+        userver::storages::postgres::ClusterHostType::kMaster,
+        "INSERT INTO bank_schema.clients(first_name, "
+        "middle_name,last_name,gender,passport_series,passport_number, "
+        "issuing,issuing_date,id_number, "
+        "birth_address,current_city,current_address, "
+        "home_number,mobile_number,email,post,place_of_work, "
+        "city_of_residence,residence_address, "
+        "family_status,citizenship,disability,retiree,monthly_income, "
+        "conscription) "
+        "VALUES($1,$2,$3,$4,$5,$6, "
+        "$7,$8,$9, "
+        "$10,$11,$12,$13,$14, "
+        "$15,$16,$17,$18,$19,$20, "
+        "$21,$22,$23,$24,$25) "
+        "RETURNING id",
+        info.first_name, info.middle_name, info.last_name,
+        static_cast<std::int16_t>(info.gender), info.passport_series,
+        info.passport_number, info.issuing, info.issuing_date, info.id_number,
+        info.birth_address, info.current_city, info.current_address,
+        info.otp_home_number, info.opt_mobile_number, info.opt_email,
+        info.opt_position, info.opt_place_of_work, info.city_of_residence,
+        info.residence_address, static_cast<std::int16_t>(info.family_status),
+        info.citizenship, static_cast<std::int16_t>(info.disability),
+        info.retiree, info.monthly_income, info.conscription);
+
+    // auto size = pg_result.Size();
+    int32_t id = pg_result[0].As<int>();
 
     // request.GetHttpResponse().SetStatus(userver::server::http::HttpStatus::kBadRequest);
-    return result.ExtractValue();
+    userver::formats::json::ValueBuilder json_builder;
+    json_builder["client"] = info;
+    json_builder["client"]["id"] = id;
+    return json_builder.ExtractValue();
   }
 
  private:
   userver::storages::postgres::ClusterPtr pg_cluster_;
 };
+
+class HttpPutClient : public userver::server::handlers::HttpHandlerJsonBase {
+ public:
+  HttpPutClient(const userver::components::ComponentConfig& config,
+                const userver::components::ComponentContext& component_context)
+      : userver::server::handlers::HttpHandlerJsonBase(config,
+                                                       component_context),
+        pg_cluster_(
+            component_context
+                .FindComponent<userver::components::Postgres>("postgres-db-1")
+                .GetCluster()) {}
+
+  userver::formats::json::Value HandleRequestJsonThrow(
+      const userver::server::http::HttpRequest& request,
+      const userver::formats::json::Value& json,
+      userver::server::request::RequestContext&) const override {
+    userver::formats::json::ValueBuilder json_builder;
+
+    return json_builder.ExtractValue();
+  }
+
+ private:
+  userver::storages::postgres::ClusterPtr pg_cluster_;
+};
+
 }  // namespace
 
 void AppendUser(userver::components::ComponentList& component_list) {
-  component_list.Append<HttpHandlerUser>();
+  component_list.Append<HttpPostClient>();
   component_list.Append<userver::components::Postgres>("postgres-db-1");
   component_list.Append<userver::clients::dns::Component>();
 }
